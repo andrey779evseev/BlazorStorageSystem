47b0fa1ef69e9b75e9f52f21dd0da2a0190e1436
andrew <andrey779evseev@gmail.com>
2022-03-05 23:32:54 +0200

    fix

--- Data/Product.cs
+++ Data/Product.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS8618
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -7,7 +8,36 @@ public class Product
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public string Id { get; set; }
     [BsonElement("name")]
-    public string? Name { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public string Name { get; set; }
+    [BsonElement("count")]
+    [BsonRepresentation(BsonType.Int32)]
+    public int Count { get; set; }
+    
+    [BsonElement("category")]
+    [BsonRepresentation(BsonType.String)]
+    public string Category { get; set; }
+    
+    [BsonElement("sale")]
+    [BsonRepresentation(BsonType.Int32)]
+    public int? Sale { get; set; }
+    
+    [BsonElement("sold")]
+    [BsonRepresentation(BsonType.Int32)]
+    public int? Sold { get; set; }
+    
+    [BsonElement("seller")]
+    [BsonRepresentation(BsonType.String)]
+    public string? Seller { get; set; }
+    
+    [BsonElement("manufacturer")]
+    [BsonRepresentation(BsonType.String)]
+    public string? Manufacturer { get; set; }
+    
+    [BsonElement("expiration_date")]
+    [BsonRepresentation(BsonType.DateTime)]
+    public DateTime ExpirationDate { get; set; }
 }
+#pragma warning restore CS8618
